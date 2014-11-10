e24b23fba759bec9b8faf9a92d2162ecbe674d35
Tarek Kazak <tarekkazak@Tarek-Kazaks-work-Computer.local>
2014-11-09 20:07:51 -0500

    #2 add edit function to tag-editor

--- app/directive/tagEditor.js
+++ app/directive/tagEditor.js
@@ -4,28 +4,47 @@
                 restrict : 'EA',
 		scope : {
 		    tagEditorTrigger: '@',
+		    editTag : '=',
+		    tagCreateCallBack : '&tagEditorCreateComplete'
+		    //tagEditorTemplate: '@'
 		},
                 link : function(scope, iElem, iAttrs) {
+			scope.editMode = false;
+			templateService.get('partials/tag-editor.html').then(function(data) {
+				return  data.data;
+			}).then(function(template) { 
 		            $(iElem).tooltip({
                                 placement:'right',
                                 template: template,
+		        	container : $(iElem),
 				trigger : !_.isUndefined(iAttrs.tagEditorTrigger) ? "manual"  : "click"
 			    });
-                        });
-                            } else {
-                            }
+			    if(!_.isUndefined(iAttrs.editTag)) {
+				scope.$watch('editTag', function(value) {
+			            scope.tag = value;
+				    scope.editMode = true;
+				    scope.title= 'editing: ' + scope.tag.label;
+				    console.log(scope.title);
+				});
+			    } else {
+				scope.title = 'create';
+			    }
+
 			    if(!_.isUndefined(scope.tagEditorTrigger) && !_.isNull(scope.tagEditorTrigger)) {
 			        scope.$watch('tagEditorTrigger', function(value) {
 				    console.log('tag trigger ' + value);
 				if(value == 'true') {
 				    $(iElem).tooltip('show');
-                    } else {
+				} else {
 				    $(iElem).tooltip('hide');
 				}
-                        });
+			        });		
 			    }
-                        });
-                    
+			});
+			scope.createNewTag = function() {
+			    scope.tagCreateCallBack(scope.tag);
+			};
+
 
                 }
             };
