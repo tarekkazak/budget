753bc8dea6f71391c015626c9712f6ce351b36b5
Tarek Kazak <tarekkazak@Tarek-Kazaks-work-Computer.local>
2014-11-17 01:33:09 -0500

    #2 linked tooltip template scope so directive scope. Created directive controller for event handling and scope.tag initializing. Refactored tag-editor template and created basic styling

--- app/directive/tagEditor.js
+++ app/directive/tagEditor.js
@@ -1,30 +1,53 @@
     angular.module('tagEditorModule', ['templateServiceModule'])
-        .directive('tagEditor', ['$timeout', 'templateService', function ($timeout, templateService) {
+    	.controller('tagEditorController', function($scope) {
+		$scope.tag= {
+		    id : new Date().getTime()
+		};
+		$scope.editMode = false;
+		$scope.createNewTag = function() {
+		    console.log("create new tag " + $scope.tag.label);
+		    $scope.tagCreateCallBack()($scope.tag);
+		};
+
+		$scope.close = function() {
+		    console.log("close tag editor");
+		    $scope.tagEditorTrigger = "false";
+		};
+	})
+        .directive('tagEditor', ['$compile', 'templateService', function ($compile, templateService) {
             return {
                 restrict : 'EA',
+		controller : 'tagEditorController',
 		scope : {
 		    tagEditorTrigger: '@',
 		    editTag : '=',
 		    tagCreateCallBack : '&tagEditorCreateComplete'
-		    //tagEditorTemplate: '@'
 		},
-                link : function(scope, iElem, iAttrs) {
-			scope.editMode = false;
+                link : function(scope, iElem, iAttrs, controller) {
 			templateService.get('partials/tag-editor.html').then(function(data) {
-				return  data.data;
+	    		    return  data.data;
 			}).then(function(template) { 
-		            $(iElem).tooltip({
+                            var compiled = $compile(template)(scope);
+			    var el = $(iElem);
+		            $(el).tooltip({
                                 placement:'right',
-                                template: template,
-		        	container : $(iElem),
+                                template: compiled,
+		        	container : 'body',
 				trigger : !_.isUndefined(iAttrs.tagEditorTrigger) ? "manual"  : "click"
 			    });
+                            $(el).on('shown.bs.tooltip', function () {
+                                console.log('show tooltip');
+				//scope.$apply();
+                             });
 			    if(!_.isUndefined(iAttrs.editTag)) {
 				scope.$watch('editTag', function(value) {
-			            scope.tag = value;
-				    scope.editMode = true;
-				    scope.title= 'editing: ' + scope.tag.label;
-				    console.log(scope.title);
+                                    if(value) {
+			                scope.tag = value;
+				        scope.editMode = true;
+				        scope.title= 'editing: ' + scope.tag.label;
+				        //console.log(scope.title);
+				        //console.log(scope.tag);
+                                    }
 				});
 			    } else {
 				scope.title = 'create';
@@ -34,16 +57,13 @@
 			        scope.$watch('tagEditorTrigger', function(value) {
 				    console.log('tag trigger ' + value);
 				if(value == 'true') {
-				    $(iElem).tooltip('show');
+				    $(el).tooltip('show');
 				} else {
-				    $(iElem).tooltip('hide');
+				    $(el).tooltip('hide');
 				}
 			        });		
 			    }
 			});
-			scope.createNewTag = function() {
-			    scope.tagCreateCallBack(scope.tag);
-			};
 
 
                 }
