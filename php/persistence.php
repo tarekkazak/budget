e73206b7875a56ea551eea9e6da8659f15448467
Tarek Kazak <tarek.kazak@gmail.com>
2014-12-28 05:13:46 +0000

    cleanup

--- php/persistence.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $data = json_decode(file_get_contents("php://input"));
-    rename ('../data/content.json' , '../data/content_'.date('Y_m_d_H_i_s').'.json' );
-    file_put_contents('../data/content.json', json_encode($data));
-
-?>
