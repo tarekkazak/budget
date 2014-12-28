4e53625e053ff0eed6ccde0ec468d7a00187bfce
Tarek Kazak <tarek.kazak@gmail.com>
2014-12-28 05:22:16 +0000

    added ne packages and grunt deps

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+    grunt.initConfig( {
+        watch : {
+            js : {
+                files : ['app/**/*.js', 'main.js', 'package.json'],
+                tasks : ['browserify:js', 'jshint:js']
+            }
+        },
+        jshint : {
+                js : ['app/**/*.js'],
+                options : {
+                    force : true
+                }
+        },
+        browserify : {
+            js: {
+                files : {'bundle.js' : ['main.js']}
+            },
+            dev : {
+                files : {'bundle.js' : ['main.js']},
+                options : {
+                    watch : true,
+                    keepAlive : true
+                }
+            }
+        },
+        nodemon: {
+            dev : {
+                script : 'server.js',
+                options : {
+                    ignore : ['app/**/*', 'node_modules/**/*', 'package.json', 'partials/**/*']
+                }
+            }
+        },
+        concurrent : {
+            target :{
+                tasks : ['watch:js', 'nodemon:dev'],
+                options : {
+                    logConcurrentOuput : true
+                }
+            }
+
+        }
+        
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.registerTask('dist', ['compass:dist', 'browserify:dist', 'copy:dist', 'cssmin:dist', 'uglify:dist']);
+    grunt.registerTask('default', ['concurrent:target']);
+};
