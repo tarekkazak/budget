6f21a57755b004435e66a10064a63b1343fea46c
Tarek Kazak <tarek.kazak@gmail.com>
2014-12-31 17:46:36 +0000

    added nullOrUndefined to utils, upgraded angular version and updated nodemon ignore

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -4,7 +4,11 @@ module.exports = (function(window) {
              _.remove(list, function (item) {
                  return item === itemToDelete;
              });
-         }
+         },
+
+        isNullOrUndefined : function(object) {
+            return _.isUndefined(object) || _.isNull(object);
+        }
     };
     window.utils = utils;
  }(window));
