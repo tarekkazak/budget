763dfda9fa7384ea67ba9ef863475ce1f8637231
Tarek Kazak <tarek.kazak@gmail.com>
2015-04-06 04:39:40 +0000

    add load existing tag for expense

--- app/directive/react/expenseList.jsx
+++ app/directive/react/expenseList.jsx
@@ -43,7 +43,8 @@ angular.module('budgetApp.directive')
                         onItemAdd : function(value) {
                             expense.tag = value;
                             budgetAppModel.updateExpense(expense.id, expense);
-                        }
+                        },
+                        items : [expense.tag]
                         
                     });
                 }
